fac18a5b8dd108e016aee1f99052b1ff53d6d047
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-04-09 08:04:05 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/BLL/Services/FriendService.cs
@@ -0,0 +1,48 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class FriendServices
+    {
+        IFriendRepository friendRepository;
+        IUserRepository userRepository;
+
+        public FriendServices()
+        {
+            userRepository = new UserRepository();
+            friendRepository = new FriendRepository();
+        }
+
+        public IEnumerable<Friend> GetFriendId(int friendId)
+        {
+            var friends = new List<Friend>();
+            friendRepository.FindAllByUserId(friendId).ToList().ForEach(m =>
+            {
+                var userUserEntity = userRepository.FindById(m.user_id);
+                var friendUserEntity = userRepository.FindById(m.friend_id);
+
+                friends.Add(new Friend(m.friend_id, friendUserEntity.email));
+            });
+
+            return friends;
+        }
+
+        public void ValidData(FriendData friendData)
+        {
+
+
+            if (!new EmailAddressAttribute().IsValid(friendData.Email))
+                throw new ArgumentNullException();
+
+            if (userRepository.FindByEmail(friendData.Email) == null)
+                throw new ArgumentNullException();
+        }
+    }
+}
---
+++ SocialNetwork/PLL/Views/AddatingToFriends.cs
@@ -0,0 +1,50 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class AddingToFriends
+    {
+        public AddingToFriends(FriendServices friendServices)
+        {
+            FriendServices = friendServices;
+        }
+
+        public FriendServices FriendServices { get; }
+
+        public void Adding()
+        {
+            var friendData = new FriendData();
+
+            
+            Console.WriteLine("Для добавления пользователя  ввидите email: ");
+            friendData.Email = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Список друзей: {0}", friendData.Email);
+
+
+            try
+            {
+                this.FriendServices.ValidData(friendData);
+                SuccessMessage.Show("Пользователь успешно добавлен");
+            }
+            catch (ArgumentNullException)
+            {
+                AlertMessage.Show("Введите корректное значение.");
+            }
+            catch (Exception)
+            {
+                AlertMessage.Show("Ошибка при добавлении в список друзей");
+            }
+            
+        }
+    }
+}
